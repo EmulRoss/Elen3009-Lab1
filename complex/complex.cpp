81fd73e11babff3f1c6b8ce2813463b8e4102fde
Arlo <eardsa@gmail.com>
2017-08-05 12:51:54 +0200

    Exercise 3.2

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -1,23 +1,26 @@
 // complex.cpp
 // Multiplying complex numbers
-
 #include <iostream>
 #include <complex> // required for the complex class
 
 using namespace std;
+using Comp = complex<float>;  
+ 
+int main(){
+    
+Comp num1{ 2.0, 2.0 }; // using C++11 uniform initialisation syntax
 
-int main()
-{
-    complex<float> num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
-    complex<float> num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+Comp num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
 
-    auto answer = num1 * num2; // using C++11 auto keyword,
-                               // answer is of type: complex<float>
+auto answer = num1 * num2; // using C++11 auto keyword,
+                        // answer is of type: complex<float>
+                        
+cout << "The answer is: " << answer << endl;
 
-    cout << "The answer is: " << answer << endl;
-    cout << "Or in a more familiar form: " << answer.real() << " + " << answer.imag() << "j" << endl;
+cout << "Or in a more familiar form: " << answer.real() << " + " <<answer.imag() << "j" << endl;
 
-    // answer++;
+
+    //answer++;
     
     //The commented line does not work because the "answer++" declaration 
     //is attempting to use the shorthand incrementation that is solely used for integer types and therefore cannot increment the float data type
