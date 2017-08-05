c6b0e9db28cc1ba283e7256e0f7626518803a5f7
Arlo <eardsa@gmail.com>
2017-08-05 12:48:00 +0200

    Exercise 3.1

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -18,6 +18,12 @@ int main()
     cout << "Or in a more familiar form: " << answer.real() << " + " << answer.imag() << "j" << endl;
 
     // answer++;
+    
+    //The commented line does not work because the "answer++" declaration 
+    //is attempting to use the shorthand incrementation that is solely used for integer types and therefore cannot increment the float data type
+
 
     return 0;
 }
+
+
